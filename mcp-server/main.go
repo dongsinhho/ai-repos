c073015422641b3b26ccb4709d17ef60d5b3f72b
dongsinhho <sinhvua@gmail.com>
2025-04-27 23:27:25 +0700

    update mcp code

--- mcp-server/main.go
+++ mcp-server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
 
+	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
 
 	"github.com/joho/godotenv"
@@ -31,10 +34,27 @@ func main() {
 	// 	return allToolsEnabled || slices.Contains(enableTools, toolName)
 	// }
 
+	tool := mcp.NewTool("hello_world",
+		mcp.WithDescription("Say hello to someone"),
+		mcp.WithString("name",
+			mcp.Required(),
+			mcp.Description("Name of the person to greet"),
+		),
+	)
 	// tools.RegisterMailTools(mcpServer)
 	// tools.RegisterAwsInsight(mcpServer)
+	mcpServer.AddTool(tool, helloHandler)
 
 	if err := server.ServeStdio(mcpServer); err != nil {
 		panic(fmt.Sprintf("Server error: %v", err))
 	}
+}
+
+func helloHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	name, ok := request.Params.Arguments["name"].(string)
+	if !ok {
+		return nil, errors.New("name must be a string")
+	}
+
+	return mcp.NewToolResultText(fmt.Sprintf("Hello, %s!", name)), nil
 }
